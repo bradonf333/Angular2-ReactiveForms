8cedc79927217dfaa2bab903a8c5f1356104cbab
bradonf333 <bradonf123@gmail.com>
2017-08-29 20:39:26 -0600

    Created button to auto fill form

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -32,4 +32,13 @@ export class CustomerComponent implements OnInit {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Bradon',
+      lastName: 'Fredrickson',
+      sendCatalog: false
+    });
+  }
+
 }
